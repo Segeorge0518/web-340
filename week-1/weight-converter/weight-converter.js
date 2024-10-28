2e6d119e11531ba5e2193a0f00d2fbd262ba8568
Segeorge0518 <segeorge0518@sbcglobal.net>
2024-10-27 22:06:11 -0400

    Week one commit

--- week-1/weight-converter/weight-converter.js
+++ week-1/weight-converter/weight-converter.js
@@ -1,10 +1,37 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Sara George
+ * Date: 10/27/2024
+ * File Name: weight-converter.js
+ * Description: pounds to kilogram converter
 */
 
 "use strict";
 
-// TODO: Implement the weight conversion logic here
+const readline = require("readline"); // imports the readline module
+
+function convertLbsToKgs() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+});
+
+//Asks for the weight in pounds
+rl.question("Enter the weight in Pounds: ", function (weight) {
+    if (isNaN(weight)) {
+        console.error("Input must be a number."); // display error message
+        process.exit(1); // exit with a non-zero error code
+}
+
+//converts pounds to kilograms
+const kgs = (weight / 2.2046).toFixed(2); 
+
+// displays the conversion of pounds to kilograms
+console.log(
+    `${weight}lbs is equivalent to ${kgs}kgs.`
+);
+
+rl.close(); // closes the readline object
+});
+}
+
+convertLbsToKgs();
