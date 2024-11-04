121cf7147b9053582a619bfc51664de6356d04ac
Segeorge0518 <segeorge0518@sbcglobal.net>
2024-11-03 20:20:30 -0500

    Week 2 assignment commit

--- week-2/cooking-app/index.js
+++ week-2/cooking-app/index.js
@@ -1,10 +1,14 @@
 /**
- * Author:
- * Date:
- * File Name:
+ * Author: Sara George
+ * Date: 11/01/2024
+ * File Name: recipes.js
  * Description:
 */
 
 // TODO: Import your module using require
+const recipes = require("recipes");
+
+const tester = require("tester");
+
 
 // TODO: Implement your CLI program here
--- week-2/cooking-app/recipes.js
+++ week-2/cooking-app/recipes.js
@@ -1,23 +1,40 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Sara George
+ * Date: 11/01/2024
+ * File Name: recipes.js
+ * Description: Recipe file
 */
 
 // Define the createRecipe function
+
 function createRecipe(ingredients) {
-  // TODO: Implement this function
+  const recipe = ["ingredient1", " ingredient2"];
+  return "Recipe created with ingredients: " + recipe.toString();
 }
 
 // Define the setTimer function
-function setTimer(minutes) {
-  // TODO: Implement this function
+function setTimer(minutes) {  
+  const durationInMilliseconds = minutes * 60000;
+
+  return "Timer set for " + (minutes) + " minutes";
+
+  setTimeout(() => {
+    console.log("Timer has finished");
+  }, durationMilliseconds);
 }
+
+setTimer(15);
 
 // Define the quit function
 function quit() {
-  // TODO: Implement this function
+  return "Program exited";
 }
 
+
 // TODO: Export the functions
+
+module.exports = {
+  createRecipe: createRecipe,
+  setTimer: setTimer,
+  quit:quit,
+};
