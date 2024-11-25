5f38ca5e16f24318ffc98b13acfa820e8af3727e
Segeorge0518 <segeorge0518@sbcglobal.net>
2024-11-24 20:49:57 -0500

    Week 5 assignment commit

--- week-5/pie-baker/src/pie.js
+++ week-5/pie-baker/src/pie.js
@@ -1,13 +1,20 @@
 /**
- * Author:
- * Date:
- * File Name:
+ * Author: Sara George
+ * Date:  11/23/2024
+ * File Name: pie.js
  * Description:
  */
 "use strict";
 
 function bakePie(pieType, ingredients) {
-  // Your code here
+  const requiredIngredients = ['flour', 'sugar', 'butter'];
+  for (const ingredient of requiredIngredients) {
+    if (!ingredients.includes (ingredient)) {
+      console.warn (`Missing essential ingredient: ${ingredient}`);
+      process.exit(1);
+    }
+  }
+  return 'You have successfully baked a ${pieType} pie!';
 }
 
 module.exports = { bakePie };
--- week-5/pie-baker/test/pie.spec.js
+++ week-5/pie-baker/test/pie.spec.js
@@ -1,7 +1,7 @@
 /**
- * Author:
- * Date:
- * File Name:
+ * Author: Sara George
+ * Date:  11/23/2024
+ * File Name: pie.spec.js
  * Description:
  */
 
@@ -9,4 +9,21 @@
 
 const { bakePie } = require("../src/pie");
 
-// Your tests here
+describe('bakePie function', () => {
+    test('should bake a pie with all essential ingredients', () => {
+        const result = bakePie('apple', ['flour', 'sugar', 'butter', 'apples']);
+        expect(result).toBe('You have successfully baked a ${pieType} pie!');
+    });
+
+    test('should bake a pie with additional ingredients', () => {
+        const result = bakePie('cherry', ['flour', 'sugar', 'butter', 'cherries', 'vanilla']);
+        expect(result).toEqual('You have successfully baked a ${pieType} pie!');
+    });
+
+    test('should call process.exit when an essential ingredient is missing', () => {
+        const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => {});
+        bakePie('blueberry', ['flour', 'sugar']);
+        expect(mockExit).toHaveBeenCalledWith(1);
+        mockExit.mockRestore();
+    });
+});
