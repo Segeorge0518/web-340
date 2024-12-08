db10a88cdf6f33eabcda17153e6073b666c9a03e
Segeorge0518 <segeorge0518@sbcglobal.net>
2024-12-08 10:36:41 -0500

    Week 6 Assignment Commit

--- week-6/fantasy-game-character-creation/src/server.js
+++ week-6/fantasy-game-character-creation/src/server.js
@@ -3,8 +3,35 @@ const url = require('url');
 
 // TODO: Implement your server here
 
+
+let character = {};
+
 const server = http.createServer((req, res) => {
-  // TODO: Implement your routes here
+    const parsedUrl = url.parse(req.url, true);
+    const pathname = parsedUrl.pathname;
+    const query = parsedUrl.query;
+
+    if (pathname === '/create-character' && req.method === 'POST') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            const { class: charClass, gender, funFact } = JSON.parse(body);
+            character = { charClass, gender, funFact };
+            res.writeHead(201);
+            res.end('Character has been created.');
+        });
+    } else if (pathname === '/confirm-character' && req.method === 'POST') {
+        res.writeHead(200);
+        res.end('Character creation has been confirmed.');
+    } else if (pathname === '/view-character' && req.method === 'GET') {
+        res.writeHead(200);
+        res.end(JSON.stringify(character));
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
 });
 
 server.listen(3000, () => {
--- week-6/fantasy-game-character-creation/test/server.spec.js
+++ week-6/fantasy-game-character-creation/test/server.spec.js
@@ -1,4 +1,73 @@
 const http = require('http');
 const server = require('../src/server');
 
-// TODO: Implement your tests here
+afterAll(() => {
+    server.close();
+});
+
+test('responds to /create-character POST request with character data', done => {
+    const options = {
+        hostname: 'localhost',
+        port: 3000,
+        path: '/create-character',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+    const req = http.request(options, res => {
+        let data = '';
+        res.on('data', chunk => {
+            data += chunk;
+        });
+        res.on('end', () => {
+            expect(res.statusCode).toBe(201);
+            expect(data).toBe('Character has been created.');
+            done();
+        });
+    });
+    req.write(JSON.stringify({ class: 'Warrior', gender: 'Male', funFact: 'Loves to fight dragons.' }));
+    req.end();
+});
+
+test('responds to /confirm-character POST request', done => {
+    const options = {
+        hostname: 'localhost',
+        port: 3000,
+        path: '/confirm-character',
+        method: 'POST',
+    };
+    const req = http.request(options, res => {
+        let data = '';
+        res.on('data', chunk => {
+            data += chunk;
+        });
+        res.on('end', () => {
+            expect(res.statusCode).toBe(200);
+            expect(data).toBe('Character creation has been confirmed.');
+            done();
+        });
+    });
+    req.end();
+});
+
+test('responds to /view-character GET request', done => {
+    const options = {
+        hostname: 'localhost',
+        port: 3000,
+        path: '/view-character',
+        method: 'GET',
+    };
+    const req = http.request(options, res => {
+        let data = '';
+        res.on('data', chunk => {
+            data += chunk;
+        });
+        res.on('end', () => {
+            expect(res.statusCode).toBe(200);
+            expect(JSON.parse(data)).toEqual({ charClass: 'Warrior', gender: 'Male', funFact: 'Loves to fight dragons.' });
+            done();
+        });
+    });
+    req.end();
+});
